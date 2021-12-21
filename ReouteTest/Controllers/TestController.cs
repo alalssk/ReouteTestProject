e6e140c959291cb720e2261222c7a4add035fd9d
DESKTOP-11KKAHC\hwangjinhyun <alalssk@gmail.com>
2021-12-21 11:03:22 +0900

    초기 커밋.

---
+++ ReouteTest/Controllers/TestController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReouteTest.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TestController : Controller
+    {
+        [HttpPost]
+        [Route("echo")]
+        public async Task<IActionResult> Echo()
+        {
+            string retBody;
+            HttpContext.Request.EnableBuffering();
+            using (var reader = new StreamReader(
+                Request.Body,
+                encoding: System.Text.Encoding.UTF8,
+                detectEncodingFromByteOrderMarks: false,
+                bufferSize: 1024,
+                leaveOpen: true
+            ))
+            {
+                retBody = await reader.ReadToEndAsync();
+            }
+
+            return Json(retBody);
+        }
+
+    }
+}
